fa1dd7080088f4d39f8b35d5958e0d567a3cf353
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-01 16:59:07 +0300

    listed movies with map func.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Search Bar</h1>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
+
