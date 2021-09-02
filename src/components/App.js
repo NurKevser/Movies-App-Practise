81359eb2c3a15b9ff27f2d8c69d6e5393f2aabe5
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-02 23:59:58 +0300

    added delete movie feature

--- src/components/App.js
+++ src/components/App.js
@@ -33,6 +33,15 @@ class App extends React.Component {
         
     }
 
+    deleteMovie = (movie) => {
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+            );
+
+            this.setState ({
+                movies: newMovieList
+            })
+    }
 
     render () {
         return(
@@ -42,7 +51,10 @@ class App extends React.Component {
                         <SearchBar />
                     </div>
                 </div>
-                <MovieList movies={this.state.movies}/>
+                <MovieList 
+                movies={this.state.movies}
+                deleteMovieProp={this.deleteMovie}
+                />
             </div>
         )
     }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,30 +1,38 @@
 import React from 'react';
 
-const MovieList =(props) => {
-    
-        return (
-            
-                <div className="row">
+const MovieList = (props) => {
 
-                    {props.movies.map((movie) => (
-                        <div className="col-lg-4" key='movie.id'>
-                        <div className="card mb-4 shadow-sm">
-                            <img className='card-img-top' src={movie.imageURL} alt="Sample Movie" />
-                            <div className="card-body">
-                                <h5 className="card-title">{movie.name}</h5>
-                                <p className="card-text">{movie.overview}</p>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <button className="btn btn-md btn-outline-danger">Delete</button>
-                                    <h2><span className='badge bg-info'>{movie.rating}</span></h2>
-                                </div>
+    /* function handleClick(event) {
+        // console.log("Button Clicked");
+        
+        console.log(event.pageY);
+    } */
+
+    return (
+
+        <div className="row">
+
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
+                    <div className="card mb-4 shadow-sm">
+                        <img className='card-img-top' src={movie.imageURL} alt="Sample Movie" />
+                        <div className="card-body">
+                            <h5 className="card-title">{movie.name}</h5>
+                            <p className="card-text">{movie.overview}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button type="button"
+                                    className="btn btn-md btn-outline-danger"
+                                    onClick={(event) => props.deleteMovieProp(movie) }>Delete</button>
+                                <h2><span className='badge bg-info'>{movie.rating}</span></h2>
                             </div>
                         </div>
                     </div>
-                    ))}
                 </div>
-            
-        )
-    
+            ))}
+        </div>
+
+    )
+
 }
 
 export default MovieList;
