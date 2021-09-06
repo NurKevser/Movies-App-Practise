563f00eed62f41db830fd056e279aafc587e4ddf
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-06 23:25:35 +0300

    added functionalty into search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+    } // varsayılan davranısı durdurduk search bara bir seyler yazıp submit edince artık sayfa yenilenmiyor
+
     render() {
         return (
             
-                <form>
+                <form onSubmit={this.handleFormSubmit}>
                     <div className="form-row mb-5">
                         <div className="col-12">
-                            <input type="text" className="form-control" placeholder="Search a movie"/>
+                            <input 
+                            onChange={this.props.searchMovieProp} 
+                            type="text" className="form-control" 
+                            placeholder="Search a movie"
+                            />
                         </div>
                     </div>
                 </form>
