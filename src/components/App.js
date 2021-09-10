a13d52eaa4cf93975d411f2968b4a1ba054cf332
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-10 15:50:49 +0300

    get movies details with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,28 @@
 import React from "react";
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import axios from 'axios';
 
 
 class App extends React.Component {
     state = {
         movies: [
-            {
-                "id": 1,
-                "name": "The Flash",
-                "rating": 8.3,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-            },
-        
-            {
-                "id": 2,
-                "name": "Interstellar",
-                "rating": 6.8,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-            },
-        
-            {
-                "id": 3,
-                "name": "Arrow",
-                "rating": 7.9,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-            },
-
-            {
-                "id": 4,
-                "name": "Rogue",
-                "rating": 7.4,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg"
-            },
-
-            {
-                "id": 5,
-                "name": "Project Power",
-                "rating": 6.7,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg"
-            },
-
-            {
-                "id": 6,
-                "name": "Superman",
-                "rating": 7.6,
-                "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-                "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-            }
+            
         ],
 
         searchQuery: ""
         
     }
 
+
+    async componentDidMount() {
+        const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=6e2e71c5acfa466c4f0c394c317110c8&language=en-US&page=1")
+        console.log(response.data.results);
+        this.setState({movies: response.data.results})
+    }
+
     deleteMovie = (movie) => {
+        axios.delete(`https://api.themoviedb.org/3${movie.id}`)
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
             );
@@ -81,7 +44,7 @@ class App extends React.Component {
 
         let filteredMovies = this.state.movies.filter(
             (movie) => {
-                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+                return movie.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
             }
         )
         return(
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -15,15 +15,15 @@ const MovieList = (props) => {
             {props.movies.map((movie) => (
                 <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
-                        <img className='card-img-top' src={movie.imageURL} alt="Sample Movie" />
+                        <img className='card-img-top' src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt="Sample Movie" />
                         <div className="card-body">
-                            <h5 className="card-title">{movie.name}</h5>
+                            <h5 className="card-title">{movie.title}</h5>
                             <p className="card-text">{movie.overview}</p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <button type="button"
                                     className="btn btn-md btn-outline-danger"
                                     onClick={(event) => props.deleteMovieProp(movie) }>Delete</button>
-                                <h2><span className='badge bg-info'>{movie.rating}</span></h2>
+                                <h2><span className='badge bg-info'>{movie.vote_average}</span></h2>
                             </div>
                         </div>
                     </div>
